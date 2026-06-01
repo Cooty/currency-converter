7280eab386939d10b52f9c383fdc1e378a6f16d1
Tamas Kuti <tamas.kuti@teclead-ventures.de>
2026-06-01 17:12:07 +0300

    chore: remove default PII collection from Sentry

--- lib/observability/observability.ts
+++ lib/observability/observability.ts
@@ -4,21 +4,11 @@ import * as Sentry from '@sentry/react-native'
 export function initObservability(remoteUrl: string) {
   Sentry.init({
     dsn: remoteUrl,
-
-    // Adds more context data to events (IP address, cookies, user, etc.)
-    // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
-    sendDefaultPii: true,
-
-    // Enable Logs
+    sendDefaultPii: false,
     enableLogs: true,
-
-    // Configure Session Replay
     replaysSessionSampleRate: 0.1,
     replaysOnErrorSampleRate: 1,
     integrations: [Sentry.mobileReplayIntegration()],
-
-    // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-    // spotlight: __DEV__,
   })
 }
 
